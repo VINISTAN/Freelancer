3c949c87434670aad5b79e32b419bfeca0e0deb4
Aravinth C <aravinthc28@gmail.com>
2022-09-07 12:20:49 +0530

    Removed Overflow error

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Moolah',
-
       theme: ThemeData(
           primaryColor: const MaterialColor(0xFF3A0072, {
             50: Color(0xFF3A0072),
